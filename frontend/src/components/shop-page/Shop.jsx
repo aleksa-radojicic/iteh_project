bce7e70483ea29d064bafe69b1f320b61a4ba607
Nebojsa Radojkovic <nebojsa.radojkovic2000@gmail.com>
2023-02-19 01:24:44 +0100

    Shop category frontend added

--- frontend/src/components/shop-page/Shop.jsx
+++ frontend/src/components/shop-page/Shop.jsx
@@ -16,6 +16,25 @@ const Shop = ({
 }) => {
   const [products_on_current_page, setProductsOnCurrentPage] = useState([]);
 
+  const[category, setCategory] = useState("");
+
+  const onOptionChange= (e)=>{
+      setCategory(e.target.value)
+  }
+
+  //useEffect za filter proizvoda
+  // useEffect(()=>{
+  //   if(category===category){
+  //       console.log(category)
+  //       axios.get(`api/shop/${category}`).
+  //       then(res=>{
+  //           console.log(res);
+  //           setProductsOnCurrentPage(res.data)
+  //       }).catch(err=>{
+  //           console.log(err);
+  //       })
+  //   }    
+  // },[category])
 
   useEffect(() => {
     axios
@@ -43,7 +62,18 @@ const Shop = ({
           <p>Here you can check our products</p>
         </div>
 
-        <div className="row mx-auto container">;
+        <div className='category-box'>
+          <select id="category" onChange={(e)=>onOptionChange(e)}>
+            <option value="">Filter by Category</option>
+            <option value="Aquariums">Aquariums</option>
+            <option value="Corals">Corals</option>
+            <option value="Food">Food</option>
+            <option value="Grow">Grow</option>
+          </select>
+          <p>Selected category: {category}</p>
+        </div>
+        
+        <div className="row mx-auto container">
           {products_on_current_page.map((product) => (
             <Product product={product} key={product.id} onAdd={onAdd} />
           ))
